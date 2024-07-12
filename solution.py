b9890276139269162d3607532a77475a0e5f9c05
Anand John Baby <anandjohnbabyv4@gmail.com>
2024-07-12 23:57:34 +0530

    Created the Ball

--- solution.py
+++ solution.py
@@ -10,7 +10,7 @@ FPS = 60
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
-
+BALL_RADIUS = 7
 class Paddle:
     COLOR = WHITE
     VEL = 4
@@ -30,7 +30,24 @@ class Paddle:
             self.y += self.VEL
 
 
-def draw(win, paddles):
+class Ball:
+    MAX_VEL = 5
+    COLOR = WHITE
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.x_vel = self.MAX_VEL
+        self.y_vel = 0
+    def draw(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
+
+    def move(self):
+        self.x = self.x_vel
+        self.y = self.y_vel
+
+
+def draw(win, paddles, ball):
     win.fill(BLACK)
 
     for paddle in paddles:
@@ -40,18 +57,19 @@ def draw(win, paddles):
         if i % 2 == 1:
             continue
         pygame.draw.rect(win, WHITE, (WIDTH//2 - 5, i, 10, HEIGHT//20))
+    ball.draw(win)
     pygame.display.update()
 
 
 def handle_paddle_movement(keys, left_paddle, right_paddle):
-    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:##############################################################
         left_paddle.move(up=True)
-    if keys[pygame.K_s] and left_paddle.y - left_paddle.VEL + left_paddle.height <= HEIGHT:
+    if keys[pygame.K_s] and left_paddle.y - left_paddle.VEL + left_paddle.height <= HEIGHT:####################################
         left_paddle.move(up=False)
 
     if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >= 0:
-        right_paddle.move(up=True)
-    if keys[pygame.K_DOWN] and right_paddle.y - right_paddle.VEL + left_paddle.height <= HEIGHT:
+        right_paddle.move(up=True)#############################################################################################
+    if keys[pygame.K_DOWN] and right_paddle.y - right_paddle.VEL + left_paddle.height <= HEIGHT:###############################
         right_paddle.move(up=False)
 
 
@@ -60,10 +78,14 @@ def main():
     clock = pygame.time.Clock()
 
     left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+    ball = Ball(WIDTH // 2, HEIGHT // 2, BALL_RADIUS)
+
     while run:
         clock.tick(FPS) #regulate speed of the while loop not more than 60 FPS
-        draw(WIN, [left_paddle, right_paddle])
+        draw(WIN, [left_paddle, right_paddle], ball)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
